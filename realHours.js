847a9857fd4e509b10f1cf4a5b283905eafc0b9c
Daniele Santoro <psyacca@gmail.com>
2013-09-18 00:02:52 +0200

    Added real total worked hours (worked + recoup)

--- realHours.js
+++ realHours.js
@@ -2,12 +2,22 @@ $(".UMenuTimeStampLabels > div").each(
     function(index){
         console.log("Index: "+index+" value: "+$(this).text());
         children = $(this).children();
-        console.log("Day: " + $(children[0]).text()); // day
-        console.log(children[1]); // worked       
-        console.log(children[2]); // required
-        //obj = $(this);
-    })
-    var total = 0;
+        if (index == $(".UMenuTimeStampLabels > div").length-1) {
+            console.log("Total Worked: " + $(children[1]).text()); // total worked
+            console.log("Total Required: " + $(children[2]).text()); // required month
+            totWorked = $(children[1]).text();
+            totRequired = $(children[2]).text();
+        }
+        else {            
+            console.log("Day: " + $(children[0]).text()); // day
+            console.log("Worked: " + $(children[1]).text()); // worked       
+            console.log("Required: " + $(children[2]).text()); // required
+            //console.log("Required (today): " + $(children[2]).text(); // required today
+            //obj = $(this);    
+        }
+        
+    })    
+    var totalRecoupMs = 0;
 $(".UMenuTimeStampContent > div").each(
     function(index){
         console.log("Row: "+index);
@@ -40,7 +50,7 @@ $(".UMenuTimeStampContent > div").each(
                         console.log("--End time: "+dEnd);
                         difference = new Date(dEnd.getTime()-dStart.getTime()).getTime();
                         console.log("Difference in ms: "+ difference);
-                        total += difference;
+                        totalRecoupMs += difference;
                         //console.log(enditem);
                     }
                     //console.log(enditemText); // controllare per fine recoup                    
@@ -52,11 +62,30 @@ $(".UMenuTimeStampContent > div").each(
 	      );        
     }
 )
-    console.log("Total ms: "+total);
-seconds = ~~(total / 1000);
+console.log("Total ms: "+totalRecoupMs);
+seconds = ~~(totalRecoupMs / 1000);
 minutes = ~~(seconds / 60);
 hours = ~~(minutes / 60);
 minutes = minutes - (hours*60);
-totalRecoup = hours+":"+minutes;
-console.log("Total recoup: "+totalRecoup);
-alert("Total recoup for this month: "+totalRecoup);
+totalRecoupStr = hours+":"+minutes;
+console.log("Total recoup: "+totalRecoupStr);
+alert(
+    "Total recoup for this month: "+totalRecoupStr+"\n"+
+    "Total worked hours for this month: "+msToStr(strToMs(totWorked)+totalRecoupMs)
+    );
+
+function strToMs(strTime){
+    var date = new Date(2000,0,1,0,0), time = strTime.split(/\:|\-/g);
+    date.setHours(time[0]);
+    date.setMinutes(time[1]);
+    return date.getTime();
+}
+function msToStr (msTime) {
+    var date = new Date(2000,0,1,0,0);
+    msTime = msTime - date.getTime();
+    seconds = ~~(msTime / 1000);
+    minutes = ~~(seconds / 60);
+    hours = ~~(minutes / 60);
+    minutes = minutes - (hours*60);
+    return hours+":"+minutes;
+}
